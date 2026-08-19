24498ef5cf69708cbfc1431c6c98d6d77c5a36b1
Anagha Nayak <anaghanayak777@gmail.com>
2026-08-19 12:17:20 +0530

    added

--- movies.js
+++ movies.js
@@ -1,23 +1,33 @@
 // Search movies
+
 const searchInput = document.getElementById("searchInput");
 const movieCards = document.querySelectorAll(".movie-card");
 
 searchInput.addEventListener("input", function () {
+
     const searchText = searchInput.value.toLowerCase();
 
     movieCards.forEach(function (card) {
-        const movieName = card.querySelector("h3").textContent.toLowerCase();
+
+        const movieName =
+            card.querySelector("h3").textContent.toLowerCase();
 
         if (movieName.includes(searchText)) {
             card.style.display = "block";
         } else {
             card.style.display = "none";
         }
+
     });
+
 });
 
 
 // Watch Now function
+
 function watchMovie(movieName) {
-    alert("Now playing: " + movieName + " 🎬");
+
+    window.location.href =
+        "watch.html?movie=" + encodeURIComponent(movieName);
+
 }
